2573bc17df3eac65171781846d21da8f8d0c5c41
kanazaya74Raccon <kanazaya74@gmail.com>
2022-03-12 19:08:35 -0300

    inx done

--- src/cpu.rs
+++ src/cpu.rs
@@ -27,6 +27,12 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_x);
     }
 
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+
     fn update_zero_and_negative_flags(&mut self, result: u8) {
         if result == 0 {
             self.processor_status = self.processor_status | 0b0000_0010;
@@ -58,6 +64,8 @@ impl CPU {
 
                 0xAA => self.tax(),
 
+                0xE8 => self.inx(),
+
                 0x00 => return,
 
                 _ => todo!(),
@@ -88,10 +96,26 @@ mod test {
     }
 
     #[test]
+    fn test_0xa9_lda_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.execute(vec![0xa9, 0xff, 0x00]);
+        assert!(cpu.processor_status & 0b1000_0000 == 0b1000_0000);
+    }
+
+    #[test]
     fn test_0xaa_tax_is_moving_from_a_to_x() {
         let mut cpu = CPU::new();
         cpu.register_a = 10;
         cpu.execute(vec![0xaa, 0x00]);
         assert_eq!(cpu.register_x, 10);
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.execute(vec![0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0)
     }
 }
