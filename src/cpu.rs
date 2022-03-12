81acfc96027455587c6747bde7f38ffc2f1f977f
kanazaya74Raccon <kanazaya74@gmail.com>
2022-03-12 11:55:15 -0300

    Adding tax

--- src/cpu.rs
+++ src/cpu.rs
@@ -19,6 +19,26 @@ impl CPU {
 
     fn lda(&mut self, value: u8) {
         self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if result == 0 {
+            self.processor_status = self.processor_status | 0b0000_0010;
+        } else {
+            self.processor_status = self.processor_status & 0b1111_1101;
+        }
+
+        if result & 0b1000_0000 != 0 {
+            self.processor_status = self.processor_status | 0b1000_0000;
+        } else {
+            self.processor_status = self.processor_status & 0b0111_1111;
+        }
     }
 
     pub fn execute(&mut self, program: Vec<u8>) {
@@ -35,6 +55,8 @@ impl CPU {
 
                     self.lda(param);
                 }
+
+                0xAA => self.tax(),
 
                 0x00 => return,
 
@@ -55,5 +77,21 @@ mod test {
         assert_eq!(cpu.register_a, 5);
         assert!(cpu.processor_status & 0b0000_0010 == 0);
         assert!(cpu.processor_status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.execute(vec![0xa9, 0x00, 0x00]);
+        assert_eq!(cpu.register_a, 0);
+        assert!(cpu.processor_status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_is_moving_from_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.execute(vec![0xaa, 0x00]);
+        assert_eq!(cpu.register_x, 10);
     }
 }
