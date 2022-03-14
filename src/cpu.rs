9107c578907502884cc3a1694992a60363f66b7c
kanazaya74Raccon <kanazaya74@gmail.com>
2022-03-14 17:29:24 -0300

    Adding cpu interacting with a memory

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,6 +4,38 @@ pub struct CPU {
     pub register_y: u8,
     pub processor_status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF]
+}
+
+trait Memory {
+    fn memory_read(&self, addr: u16) -> u8; 
+
+    fn memory_write(&mut self, addr: u16, data: u8);
+    
+    fn memory_read_u16(&self, pos: u16) -> u16 {
+        let lo = self.memory_read(pos) as u16;
+        let hi = self.memory_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn memory_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.memory_write(pos, lo);
+        self.memory_write(pos + 1, hi);
+    }
+}
+
+
+impl Memory for CPU {
+    
+    fn memory_read(&self, addr: u16) -> u8 { 
+        self.memory[addr as usize]
+    }
+
+    fn memory_write(&mut self, addr: u16, data: u8) { 
+        self.memory[addr as usize] = data;
+    }
 }
 
 impl CPU {
@@ -14,7 +46,27 @@ impl CPU {
             register_y: 0,
             processor_status: 0,
             program_counter: 0,
+            memory: [0; 0xFFFF]
         }
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.processor_status = 0;
+ 
+        self.program_counter = self.memory_read_u16(0xFFFC);
+    }
+ 
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.memory_write_u16(0xFFFC, 0x8000);
+    }
+ 
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.execute()
     }
 
     fn lda(&mut self, value: u8) {
@@ -47,16 +99,15 @@ impl CPU {
         }
     }
 
-    pub fn execute(&mut self, program: Vec<u8>) {
-        self.program_counter = 0;
+    pub fn execute(&mut self) {
     
         loop {
-            let opscode = program[self.program_counter as usize];
+            let opscode = self.memory_read(self.program_counter);
             self.program_counter += 1;
     
             match opscode {
                 0xA9 => {
-                    let param = program[self.program_counter as usize];
+                    let param = self.memory[self.program_counter as usize];
                     self.program_counter += 1;
 
                     self.lda(param);
@@ -81,7 +132,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_is_loading_accumulator() {
         let mut cpu = CPU::new();
-        cpu.execute(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 5);
         assert!(cpu.processor_status & 0b0000_0010 == 0);
         assert!(cpu.processor_status & 0b1000_0000 == 0);
@@ -90,7 +141,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.execute(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert_eq!(cpu.register_a, 0);
         assert!(cpu.processor_status & 0b0000_0010 == 0b10);
     }
@@ -98,23 +149,22 @@ mod test {
     #[test]
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.execute(vec![0xa9, 0xff, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xff, 0x00]);
         assert!(cpu.processor_status & 0b1000_0000 == 0b1000_0000);
     }
 
     #[test]
     fn test_0xaa_tax_is_moving_from_a_to_x() {
         let mut cpu = CPU::new();
-        cpu.register_a = 10;
-        cpu.execute(vec![0xaa, 0x00]);
-        assert_eq!(cpu.register_x, 10);
+        cpu.load_and_run(vec![0xa9, 0x05, 0xaa, 0x00]);
+        assert_eq!(cpu.register_x, 5);
     }
 
     #[test]
     fn test_inx_overflow() {
         let mut cpu = CPU::new();
         cpu.register_x = 0xff;
-        cpu.execute(vec![0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xff, 0xaa, 0xe8, 0x00]);
 
         assert_eq!(cpu.register_x, 0)
     }
